c882a19dbc6bae64ad6d031f5f60930c64bd8fc9
jroeland <jaime.roeland@asoreco.com>
2016-10-23 18:39:29 +0200

    Fixed bug when stopping the services

--- project/build.sh
+++ project/build.sh
@@ -1,4 +1,6 @@
 docker-compose up -d
 docker-compose run web python /home/docker/code/app/manage.py migrate
+docker-compose run web python /home/docker/code/app/manage.py loaddata /home/docker/code/app/admin/fixtures/initial_data.json
 docker-compose run web python /home/docker/code/app/manage.py loaddata /home/docker/code/app/customers/fixtures/initial_data.json
 docker-compose run web python /home/docker/code/app/manage.py loaddata /home/docker/code/app/products/fixtures/initial_data.json
+docker-compose run web python /home/docker/code/app/manage.py loaddata /home/docker/code/app/discounts/fixtures/initial_data.json
