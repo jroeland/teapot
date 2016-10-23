a0b97fa998075a0fa114d5554d47f563e160b887
jroeland <jaime.roeland@asoreco.com>
2016-10-23 18:40:00 +0200

    added script to build the project using pip only

---
+++ project/easy_build.sh
@@ -0,0 +1,11 @@
+#Use easy build when TEST = True in the settings file.
+#Then we can use the default django db (sqlite) and run
+#the project using django's built-in server
+
+#Install dependencies
+pip install -r web/app/requirements.txt
+python web/app/manage.py migrate
+python web/app/manage.py loaddata web/app/admin/fixtures/initial_data.json
+python web/app/manage.py loaddata web/app/customers/fixtures/initial_data.json
+python web/app/manage.py loaddata web/app/products/fixtures/initial_data.json
+python web/app/manage.py loaddata web/app/discounts/fixtures/initial_data.json
