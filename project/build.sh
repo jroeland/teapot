c1b5f48567ae4563f32aac8a677b9463cbf82720
jroeland <jaime.roeland@asoreco.com>
2016-10-24 01:58:51 +0200

    Temporal fix for nginx not starting properly after build

--- project/build.sh
+++ project/build.sh
@@ -1,1 +1,8 @@
 docker-compose up -d
+
+#For some reason, nginx doesnt start properly after building
+#Restarting the containers seems to fix it
+echo "Stop containers..."
+docker stop djangoApp
+docker stop djangoAppDB
+docker-compose up -d
