bbd58c9fc635b2fa29e71df9e21e275f42f51cfe
jroeland <jaime.roeland@asoreco.com>
2016-10-24 02:23:40 +0200

    Added temp fix for nginx not starting correctly.

--- project/rebuildAll.sh
+++ project/rebuildAll.sh
@@ -3,3 +3,10 @@ docker stop djangoAppDB
 docker rm djangoApp djangoAppDB --force
 docker rmi epicproject_web postgres --force
 docker-compose up -d
+
+#For some reason, nginx doesnt start properly after building
+#Restarting the containers seems to fix it
+docker stop djangoApp
+docker stop djangoAppDB
+docker-compose up -d
+echo "Service should be up and running."
